b04543703853c638f5d5505275610893cdaa7226
josepg1994 <yepaa17@gmail.com>
2018-04-23 13:18:49 +0200

    Commit inicial

--- src/saludo.java
+++ src/saludo.java
@@ -4,6 +4,7 @@ public class saludo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.print("pa k quieres saber eso jaja salu2");
+System.out.println("actualizado");
 	}
 
 }
